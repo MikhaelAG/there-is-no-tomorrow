fe1d592daceb41178ab5d24d1dad3bb8c3d0b7bf
MikhaelAG <130944619+MikhaelAG@users.noreply.github.com>
2024-05-15 15:56:32 -0500

    there-is-no-tomorrow-commit

--- modules/vpc_subnets/main.tf
+++ modules/vpc_subnets/main.tf
@@ -1,1 +1,12 @@
-
+resource "google_compute_subnetwork" "sub-sg" {
+  name = "${var.name}-sub-sg"
+  network = var.network_id
+  ip_cidr_range = var.ip_cidr_range
+  region = var.region
+  private_ip_google_access = var.private_ip_google_access
+  log_config {
+    aggregation_interval = "INTERVAL_5_SEC"
+    flow_sampling        = 0.5
+    metadata             = "INCLUDE_ALL_METADATA"
+  }
+}
---
+++ modules/vpc_subnets/variables.tf
@@ -0,0 +1,12 @@
+variable "network_id"{}
+
+variable "ip_cidr_range"{}
+
+variable "region"{
+    type = string
+    description = "Gcp region"
+}
+
+variable "name"{}
+
+variable "private_ip_google_access"{}
